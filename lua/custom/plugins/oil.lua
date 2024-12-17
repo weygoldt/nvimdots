11f3c87506507335f37d8030dcb766f1f9a55373
weygoldt <88969563+weygoldt@users.noreply.github.com>
2024-12-17 12:02:50 +0100

    default show hidden files in oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,7 +1,38 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          local m = name:match '^%.'
+          return m ~= nil
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+        -- Sort file names with numbers in a more intuitive order for humans.
+        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+        natural_order = 'fast',
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+          -- sort order can be "asc" or "desc"
+          -- see :help oil-columns to see which columns are sortable
+          { 'type', 'asc' },
+          { 'name', 'asc' },
+        },
+        -- Customize the highlight group for the file name
+        highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+          return nil
+        end,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { noremap = true, silent = true, desc = 'Toggle Oil' })
   end,
 }
