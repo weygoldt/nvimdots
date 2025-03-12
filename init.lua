ac1537d486925538fa1729898fb0e4111f3d37af
weygoldt <88969563+weygoldt@users.noreply.github.com>
2025-03-12 20:40:34 +0100

    tmux copy paste remote

--- init.lua
+++ init.lua
@@ -118,6 +118,27 @@ vim.opt.showmode = false
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
+-- EXPERIMENTAL FOR TMUX STUFF:
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg '', '\n'),
+    vim.fn.getregtype '',
+  }
+end
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = paste,
+    ['*'] = paste,
+  },
+}
+
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
 
 -- Enable break indent
 vim.opt.breakindent = true
