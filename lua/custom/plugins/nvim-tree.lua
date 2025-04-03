07c9932f78252be0d134647df0c1fb346049eb2d
weygoldt <88969563+weygoldt@users.noreply.github.com>
2025-04-03 12:39:44 +0200

    simplyfy config, added harpoon

--- lua/custom/plugins/nvim-tree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'nvim-tree/nvim-tree.lua',
-  config = function()
-    require('nvim-tree').setup()
-    vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>', { noremap = true, desc = 'Toggle file [e]xplorer' })
-  end,
-}
